a058e4b17459b6948c70d4a2318addd4cd01421b
Akash Rajpurohit <akashrajpurohit.009@gmail.com>
2020-07-09 16:00:26 +0530

    test

---
+++ supertokens/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const supertokens = require('supertokens-node');
+
+const app = express();
+
+app.post("/login", async function (req, res) {
+	// check for user credentials..
+
+	let userId = "User1";
+	let jwtPayload = {name: "spooky action at a distance"};
+	let sessionData = {awesomeThings: ["programming", "javascript", "supertokens"]};
+
+	await supertokens.createNewSession(res, userId, jwtPayload, sessionData);
+	
+	res.send("logged in");
+});
+
+
+app.listen(4200, () => console.log('Started'))
